2d8ee03c3b9f8fc6e58e13ea1b1097806a574843
ℂᎾℂᎾ Ѕℐℛ <96687367+cocosir007@users.noreply.github.com>
2022-01-13 10:06:13 +0530

    Coco sir .js

--- Pinky.js
+++ Pinky.js
@@ -1,4 +1,4 @@
-/* codded by afnanplk
+/* codded by COCO-SIR
 */
 
 const { Sequelize } = require('sequelize');
@@ -12,6 +12,6 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
  PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
- PHONE: process.env.NUMBER === undefined ? '+919895828468' : process.env.NUMBER,   
- OA_NAME: process.env.DEPLOYER === undefined ? 'Amalser' : process.env.DEPLOYER,    
+ PHONE: process.env.NUMBER === undefined ? '+919072460920' : process.env.NUMBER,   
+ OA_NAME: process.env.DEPLOYER === undefined ? 'COCO-SIR' : process.env.DEPLOYER,    
 };
